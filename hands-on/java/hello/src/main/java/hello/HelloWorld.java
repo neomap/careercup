7166fa9a218434bd83882f012f5cd02236cecf17
Andy Zhang <leowa@outlook.com>
2016-03-30 11:31:28 +0800

    add java gradle build hello

---
+++ hands-on/java/hello/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+
+    Logger logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld1");
+    logger.debug("Hello world.");
+
+  }
+}
